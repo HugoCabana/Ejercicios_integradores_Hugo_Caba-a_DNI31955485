4d14d9955a3fc614205bc7e0dd1bd5e42a69999b
Hugo Cabana <hugocabana21@gmail.com>
2023-03-20 19:59:12 -0300

    cambie comentarios

--- ejercicio1-2-3-4.py
+++ ejercicio1-2-3-4.py
@@ -1,3 +1,5 @@
+#1. Escribir una función que calcule el máximo común divisor entre dos números.
+
 def calcular_mcd(a, b):
     """
     Esta función calcula el máximo común divisor (MCD) entre dos números utilizando el algoritmo de Euclides.
@@ -7,20 +9,28 @@ def calcular_mcd(a, b):
     return a
 
 
-mcd = calcular_mcd(2, 10) 
-print(mcd) 
+mcd = calcular_mcd(72, 108) 
+print(mcd)
+print("") 
+
+#2. Escribir una función que calcule el mínimo común múltiplo entre dos números
 
 def calcular_mcm(a, b): 
     """ Esta función calcula el mínimo común múltiplo (mcm) entre dos números utilizando el algoritmo de Euclides. """ 
     mcd = calcular_mcd(a, b) 
     return (a * b) / mcd
 
-mcm = calcular_mcm(2, 10) 
-print(mcm) 
+mcm = calcular_mcm(72, 108) 
+print(mcm)
+print("") 
 
+# 3. Escribir un programa que reciba una cadena de caracteres y devuelva un diccionario con cada palabra 
+#  que contiene y la cantidad de veces que aparece (frecuencia).
 
 def contar_palabras(cadena):
-     """ Esta función recibe una cadena de caracteres y devuelve un diccionario con la frecuencia de cada palabra en la cadena. """ 
+     """ Esta función recibe una cadena de caracteres y devuelve un diccionario con la frecuencia de cada
+        palabra en la cadena. 
+    """ 
      palabras = cadena.split() # Separa la cadena en palabras 
      frecuencia = {} 
      for palabra in palabras: 
@@ -30,6 +40,26 @@ def contar_palabras(cadena):
             frecuencia[palabra] = 1 
      return frecuencia
 
-cadena = "Codo a Codo - Curso Django 2023 - comision 23316 - 23316" 
+cadena = "Python Codo a Codo Curso Django Python 2023 comision 23316 23316 Python python" 
 frecuencia = contar_palabras(cadena) 
-print(frecuencia) # Imprime {'Hola': 1, 'mundo': 3, 'hola': 2, 'HOLA': 1}
+print(frecuencia)
+print("")
+
+
+# 4. Escribir una función que reciba una cadena de caracteres y devuelva un diccionario con cada 
+# palabra que contiene y la cantidad de veces que aparece (frecuencia). Escribir otra función que
+#   reciba el diccionario generado con la función anterior y devuelva una tupla con la palabra más 
+#  repetida y su frecuencia.
+
+def palabra_mas_repetida(frecuencia):
+    """
+    Esta función recibe un diccionario con la frecuencia de cada palabra y devuelve una tupla con la 
+    palabra más repetida y su frecuencia.
+    """
+    palabra = max(frecuencia, key=frecuencia.get)
+    frecuencia_max = frecuencia[palabra]
+    return (palabra, frecuencia_max)
+
+frecuencia = contar_palabras(cadena) 
+palabra, frecuencia_max = palabra_mas_repetida(frecuencia)
+print(palabra, frecuencia_max)
